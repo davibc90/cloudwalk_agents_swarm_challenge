e617cb368f867b14b3cb93a9ed8b0c7265a18856
Davi Carneiro <davibc.16@gmail.com>
2025-09-30 13:55:56 -0300

    refinement in instructions

--- prompts/customer_service_agent_prompt.py
+++ prompts/customer_service_agent_prompt.py
@@ -5,14 +5,13 @@ customer_service_agent_prompt = """<role>
 
 <user_info>
 - retrieve_user_info tool: Retrieves user information from the database
-- Must be done before any other action in order to obtain general user data
+- Must be done before any other action in order to obtain general user data for problem analysis
 </user_info>
 
 <new_support_call>
 1. new_support_call tool: Register a new support call for human team assessment
-2. Must be used in the following cases only:
-   - user cannot log in to the account
-   - user wants to update registration data
+2. Must be used in the following restricted cases:
+   - user cannot log in to the account, even after being instructed on how to do it
    - any error message shown in the credit card machine screen
 </new_support_call>
 
